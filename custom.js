be7586cd344f6512b5b1f35efd05bfe7dcdb172d
dpdahal <dp48dahal@gmail.com>
2024-12-06 10:01:29 +0545

    first commit

---
+++ custom.js
@@ -0,0 +1,59 @@
+"use strict";
+// username ="sophia"
+// console.log(username)
+
+// what is variable?
+// variable is a container for storing data values
+
+// rules for declaring variable
+// 1. variable name can only contain letters,
+ // underscore and dollar sign
+ 
+//  age=10
+//  console.log(age)
+// myusername='sophia'
+
+
+// myUserName > camel case -> function
+// my_user_name -> snake case -> variable
+// MyUserName -> pascal case -> OOP class
+
+// var x=10
+// var y=20
+// console.log(x+y)
+
+// var, let, const
+
+// const x=40
+// // x=50
+// console.log(x)
+
+// let user_name ='ram'
+// let email ='ram@gmail.com'
+// document.getElementById('info').innerHTML=user_name
+// document.getElementById('email_id').innerHTML=email
+// // rol_num, name, email, address, phone
+
+// document.getElementById()
+// document.getElementsByName()
+// document.getElementsByClassName()
+// document.getElementsByTagName()
+// document.querySelector();
+// document.querySelectorAll()
+
+// let x=10
+// let y=40
+
+// alert('hello world')
+
+// confirm('are you sure?')
+// window
+// let x =prompt('enter your age')
+// if(x>18){
+//     alert("Hello")
+// }else{
+//     alert("bye")
+// }
+
+
+// difference between for while and do while loop
